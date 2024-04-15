710a0a3ef72cb5aef815c1b64d8019bea49620e8
Carlo Frenzel <frenzel@uni-potsdam.de>
2024-04-15 17:22:57 +0200

    no message

---
+++ Spring Boot/src/main/java/SE01/Unbekannt/UnbekanntApplication.java
@@ -0,0 +1,13 @@
+package SE01.Unbekannt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnbekanntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnbekanntApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/src/test/java/SE01/Unbekannt/UnbekanntApplicationTests.java
@@ -0,0 +1,13 @@
+package SE01.Unbekannt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnbekanntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
